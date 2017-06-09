e0d7f33f79dc00a97010efd0a6df81236b9f3d0f
Jose <jfmandroid@gmail.com>
2017-06-09 00:38:38 +0000

    Update contanct and user models

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,7 @@
 class Contact < ApplicationRecord
+  belongs_to :user
+  
+  validates :first_name, length: { maximum: 64 }, presence: true
+  validates :last_name, length: { maximum: 64 }
+  validates :email, length: { maximum: 128 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :contacts
+  
+  validates :name, length: { maximum: 64 }, presence: true
+  validates :email, length: { maximum: 128 }, presence: true
+  validates :password_diggest, length: { maximum: 128 }
 end
