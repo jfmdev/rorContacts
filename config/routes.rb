50503c03b2d611c5f5e4c1ad79c622ef7983d6aa
Jose <jfmandroid@gmail.com>
2017-06-09 22:21:15 +0000

    Update content of about page and set default page

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   resources :users
   resources :contacts
+  
+  root 'static_pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
